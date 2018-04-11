3f67cd90d76ab82d65afc3c155728e3d349a1107
yongyuwen <xthele@hotmail.com>
2018-04-11 20:33:16 +0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -9,9 +9,10 @@ training_data, validation_data = data
 num_steps = len(training_data[0][0])
 num_classes = len(training_data[1][0])
 assert num_classes == 16
-epochs = 40
-checkpoint = "./tmp/rnn_model_3.ckpt"
-save="./saves/LSTM_BatchSize96_DropOutHalf"
+epochs = 20
+
+#checkpoint = "./tmp/rnn_model_3.ckpt"
+#save="./saves/LSTM_BatchSize96_DropOutHalf"
 
 #Create Data Pipeline
 pipeline = data_pipeline(batch_size=64, shuffle_buffer_size=100000)
@@ -21,5 +22,5 @@ net = RNN('GRU', state_size=427, num_steps=num_steps, num_layers=1, num_classes=
 
 #Train the model
 with tf.Session() as sess:
-    net.train(sess, epochs=epochs, learning_rate= 3e-4, pipeline=pipeline, training_data=training_data, validation_data=validation_data,
-              dropout=0.3, checkpoint=checkpoint, save=None)
+    net.train(sess, epochs=epochs, learning_rate= 1e-3, pipeline=pipeline, training_data=training_data, validation_data=validation_data,
+              SGDR=True, dropout=0.3, checkpoint=None, save=None)
